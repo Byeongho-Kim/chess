ce30cdceee5647b60bbe479f2ca743831c7100f1
Byeongho-Kim <kimb60@byu.edu>
2025-10-22 00:24:35 -0600

    basic structure of Services

---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,25 @@
+package service;
+
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.UserData;
+import java.util.UUID;
+
+public class UserService {
+    private DataAccess dataAccess;
+
+    public UserService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    public RegisterResult register(String username, String password, String email) throws ServiceException {}
+
+    public LoginResult login(String username, String password) throws ServiceException {}
+
+    public void logout(String authToken) throws ServiceException {}
+
+    public static class RegisterRequest {}
+
+    public static class LoginRequest {}
+}
