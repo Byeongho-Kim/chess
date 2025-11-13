c494dbc13146d0cfe56f4ced22919530905539d8
Byeongho-Kim <kimb60@byu.edu>
2025-11-13 15:04:39 -0700

    petshop patterned test infrastructure

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -2,17 +2,20 @@ package client;
 
 import org.junit.jupiter.api.*;
 import server.Server;
+import model.AuthData;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
     }
 
     @AfterAll
@@ -20,6 +23,19 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    void clearDatabase() throws Exception {
+        // Clear database before each test
+        try {
+            var request = java.net.http.HttpRequest.newBuilder()
+                    .uri(new java.net.URI("http://localhost:" + server.port() + "/db"))
+                    .DELETE()
+                    .build();
+            java.net.http.HttpClient.newHttpClient().send(request,
+                    java.net.http.HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+        }
+    }
 
     @Test
     public void sampleTest() {
